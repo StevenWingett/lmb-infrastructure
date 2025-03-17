e007a9edd8b1b13709b078924fcb476c29da46c2
Steven Wingett <9609778+StevenWingett@users.noreply.github.com>
2025-03-17 12:31:00 +0000

    Create install_R_modules.sh
    Bash script to install R modules on the Cell Biology Xeon workstation.

---
+++ Cell_Biology_Xeon/install_R_modules.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Script to set up R modules on on Cell Biology Xeon
+# Used for R version 4.4.3 (2025-02-28) -- "Trophy Case"
+# R was added to the PPA (https://cran.r-project.org/bin/linux/ubuntu/#root) 
+#
+# The exiting R modules were removed (to identify these locations: execute in R .libPaths())
+# R was unistalled (sudo apt remove r-base-core)
+# R was re-installed (sudo apt install r-base-core)
+#
+# Run this script in sudo mode to install R modules globally
+#
+# Install with Rscript
+Rscript -e 'install.packages("Matrix", dependencies=TRUE)'
+Rscript -e 'install.packages("viridis", dependencies=TRUE)'
+Rscript -e 'install.packages("ggplot2", dependencies=TRUE)'
+Rscript -e 'install.packages("ragg", dependencies=TRUE)'
+Rscript -e 'install.packages("rvest", dependencies=TRUE)'
+Rscript -e 'install.packages("xml2", dependencies=TRUE)'
+Rscript -e 'install.packages("systemfonts", dependencies=TRUE)'
+Rscript -e 'install.packages("textshaping", dependencies=TRUE)'
+Rscript -e 'install.packages("viridisLite", dependencies=TRUE)'
+Rscript -e 'install.packages("tidyverse", dependencies=TRUE)'
+Rscript -e 'install.packages("rlist", dependencies=TRUE)'
+Rscript -e 'install.packages("Rtsne", dependencies=TRUE)'
+Rscript -e 'install.packages("umap", dependencies=TRUE)'
+Rscript -e 'install.packages("httpgd", dependencies=TRUE)'
+Rscript -e 'install.packages("ggvenn", dependencies=TRUE)'
+Rscript -e 'install.packages("Seurat", dependencies=TRUE)'
+Rscript -e 'install.packages("irlba", dependencies=TRUE)'
+Rscript -e 'install.packages("igraph", dependencies=TRUE)'
+Rscript -e 'install.packages("leiden", dependencies=TRUE)'
+Rscript -e 'install.packages("patchwork", dependencies=TRUE)'
+Rscript -e 'install.packages("harmony", dependencies=TRUE)'
+Rscript -e 'install.packages("devtools", dependencies=TRUE)'
+Rscript -e 'install.packages("reticulate", dependencies=TRUE)'
+
+Rscript -e 'install.packages("BiocManager", dependencies=TRUE)'
+
+
+# Install with BiocManager
+Rscript -e 'BiocManager::install("biomaRt")'
+Rscript -e 'BiocManager::install("PCAtools")'
+Rscript -e 'BiocManager::install("ensembldb")'
+Rscript -e 'BiocManager::install("scran")'
+Rscript -e 'BiocManager::install("DESeq2")'
+Rscript -e 'BiocManager::install("scDblFinder")'
+Rscript -e 'BiocManager::install("celda")'
+Rscript -e 'BiocManager::install("DropletUtils")'
+Rscript -e 'BiocManager::install("AnnotationHub")'
+
+
+# The following requested should have been already installed as dependencies
+# for the modules listed above
+#
+# scater
+# BiocParallel
+
+
+echo R Modules Installed

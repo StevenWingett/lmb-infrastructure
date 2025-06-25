e72cd4595c222b0b9a97271bb5b8afaa0546170f
Steven Wingett <9609778+StevenWingett@users.noreply.github.com>
2025-06-25 15:29:46 +0100

    Create install_python3_modules.sh

---
+++ Cell_Biology_Xeon/install_python3_modules.sh
@@ -0,0 +1,8 @@
+# Suggested way to run the script
+# From inside the JupyterHub Terminal run the command
+# sudo -E intall_python3_module.sh
+
+pip install numpy
+pip install pandas
+
+pip install cellpose

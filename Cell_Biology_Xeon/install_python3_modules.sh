b513ffebc5061202a71a7c6f7f5be3ea589f7797
Steven Wingett <9609778+StevenWingett@users.noreply.github.com>
2026-05-08 16:35:25 +0100

    Update install_python3_modules.sh to include more packages
    Added additional Python packages for data analysis and visualization.

--- Cell_Biology_Xeon/install_python3_modules.sh
+++ Cell_Biology_Xeon/install_python3_modules.sh
@@ -1,13 +1,19 @@
 # Suggested way to run the script
 # From inside the JupyterHub Terminal run the command
-# sudo -E intall_python3_module.sh
+# sudo -E bash intall_python3_module.sh
 
 pip install numpy
 pip install pandas
+pip install matplotlib
 pip install seaborn
-pip install scanpy
-pip install scipy
 
+pip install scipy
+pip install plotly
+pip install openpyxl
+pip install py3Dmol
+pip install biopython
+
+pip install 'scanpy[leiden]'
 pip install cellpose
 pip install triku
 pip install harmonypy
@@ -20,3 +26,5 @@ pip install pybedtools
 pip install pairtools
 
 pip install scikit-image
+
+pip install ipywidgets
